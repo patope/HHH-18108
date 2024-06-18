9876fd4c4724bc590bdbe096f7cd58e0873619b5
Tomi Pakarinen <tomi.pakarinen@resys.io>
2024-06-18 18:01:47 +0300

    Initial commit

---
+++ src/main/java/com/example/hibernate_issue/DataEntity.java
@@ -0,0 +1,12 @@
+package com.example.hibernate_issue;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class DataEntity {
+
+  @Id
+  long id;
+
+}
---
+++ src/main/java/com/example/hibernate_issue/HibernateIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateIssueApplication.class, args);
+	}
+
+}
